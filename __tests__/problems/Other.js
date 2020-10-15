86ce65b6b4bc5e044e9f829f4f1439c26b7c6d86
Ariel Alvarez <arielalvarez88@gmail.com>
2020-10-15 07:02:02 -0400

    skyline problem

--- __tests__/problems/Other.js
+++ __tests__/problems/Other.js
@@ -1,4 +1,4 @@
-const {factorial} = require("../../src/problems/Other");
+const {factorial, maxIncreaseKeepingSkyline} = require("../../src/problems/Other");
 describe("Other problems", ()=>{
 
     describe("Write a function to calculate the factorial of a number", ()=>{
@@ -7,4 +7,17 @@ describe("Other problems", ()=>{
                 expect(factorial(5)).toBe(120);
             })
     })
+
+    describe("Leetcode: 807. Max Increase to Keep City Skyline", ()=>{
+       describe("Input provided", ()=>{
+            let skyline, result;
+            beforeEach(()=>{
+                skyline = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]];
+                result = 35;
+            });
+            test("My first solution: OtherProblems.maxIncreaseKeepingSkyline", ()=>{
+                expect(maxIncreaseKeepingSkyline(skyline)).toEqual(result)
+            });
+       });
+    });
 });
--- src/problems/Other.js
+++ src/problems/Other.js
@@ -8,4 +8,31 @@ function factorial(x){
     return x * factorial(x-1);
 }
 
-module.exports = {factorial};
+ const maxIncreaseKeepingSkyline = function(grid) {
+        let maxRows = [];
+        let maxCols = [];
+        let col = 0;
+        let allowedToIncreaseSum = 0;
+        for(let i =0; i < grid.length; i++){
+            let maxRow = Math.max.apply(Math, grid[i]);
+            let colArr = [];
+            for(let j = 0; j < grid.length; j++){
+                colArr.push(grid[j][col]);
+            }
+            let maxCol = Math.max.apply(Math, colArr);
+            maxRows.push(maxRow);
+            maxCols.push(maxCol);
+            col++;
+        }
+        for(let i = 0; i < grid.length; i++){
+            for(let j = 0; j < grid[i].length; j ++){
+                let maxAllowed = Math.min(maxRows[i], maxCols[j])
+                if(grid[i][j] < maxAllowed){
+                    allowedToIncreaseSum += maxAllowed - grid[i][j];
+                }
+            }
+        }
+        return allowedToIncreaseSum;
+};
+
+module.exports = {factorial, maxIncreaseKeepingSkyline};
